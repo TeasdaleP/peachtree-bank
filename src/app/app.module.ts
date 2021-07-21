671cbe12da2c827ed3e3f264cc459d6f17fecef5
TeasdaleP <pteasdale55@gmail.com>
2021-07-21 20:38:47 +0100

    added home page, added bb-ui module and added assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './components/main/app.component';
+import { AppComponent } from './components/pages/main/app.component';
 import { SharedModule } from './components/shared/shared.module';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { reducers } from './ngrx';
 import { EffectsModule } from '@ngrx/effects';
 import { TransactionEffects } from './ngrx/effects/transaction.effects';
+import { BbUIModule } from './components/bb-ui/bb-ui.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent  
   ],
   imports: [
     BrowserModule,
@@ -20,6 +21,7 @@ import { TransactionEffects } from './ngrx/effects/transaction.effects';
     StoreModule.forRoot(reducers),
     StoreDevtoolsModule.instrument(),
     EffectsModule.forRoot([TransactionEffects]),
+    BbUIModule,
     SharedModule,
     AppRoutingModule
   ],
