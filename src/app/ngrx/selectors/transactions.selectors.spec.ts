5abf8a2c8c61ac02f8802d3c2e9841940e1d754a
TeasdaleP <pteasdale55@gmail.com>
2021-07-24 12:10:20 +0100

    unit test selector

--- src/app/ngrx/selectors/transactions.selectors.spec.ts
+++ src/app/ngrx/selectors/transactions.selectors.spec.ts
@@ -1,24 +1,71 @@
+import { TestBed } from "@angular/core/testing";
+import { MockStore, provideMockStore } from "@ngrx/store/testing";
 import { mockTransactions } from "src/app/helpers/mock.data";
 import { State } from "..";
 
-import * as selector from './transactions.selectors';
+import { getBalance } from './transactions.selectors';
 
 describe('Transaction Selectors', () => {
-    let mockState: State = {
+    let store: MockStore<State>;
+    
+    const initialState: State = {
         transactions: mockTransactions
     };
+    
+    it('Should get a total of transactions when calling getBalance', () => {
+        const result = getBalance.projector(
+            initialState.transactions
+        );
 
-    it('Should be able to use getTransactions to get a list of transactions', () => {
-        const result = selector.getTransactions;
-        expect(result.length).toEqual(1);
+        expect(result).toEqual(9999);
+    });
+
+    it('Should see strings and numbers converted into getBalance total', () => {
+        let mockState : State = {
+            transactions: [
+                {
+                    categoryCode: 'CODE-12345',
+                    dates: {
+                        valueDate: 123456789890
+                    },
+                    transaction: {
+                        amountCurrency: {
+                            amount: 100,
+                            currencyCode: 'EUR'
+                        },
+                        type: 'Salary',
+                        creditDebitIndicator: 'CRDT'
+                    },
+                    merchant: {
+                        name: 'Peachtree Bank',
+                        accountNumber: '12345678'
+                    }
+                },
+                {
+                    categoryCode: 'CODE-12345',
+                    dates: {
+                        valueDate: 123456789890
+                    },
+                    transaction: {
+                        amountCurrency: {
+                            amount: '100',
+                            currencyCode: 'EUR'
+                        },
+                        type: 'Salary',
+                        creditDebitIndicator: 'CRDT'
+                    },
+                    merchant: {
+                        name: 'Peachtree Bank',
+                        accountNumber: '12345678'
+                    }
+                }
+            ]
+        }   
         
+        const result = getBalance.projector(
+            mockState.transactions
+        )
+
+        expect(result).toEqual(200);
     });
-
-    it('Should be able to get a getBalance of the transactions', () => {
-
-    });
-
-    it('Should see Nan types excluded from the getBalance total', () => {
-
-    })
 });
