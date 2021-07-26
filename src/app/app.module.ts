03bc2a9923ca419bc4169cfcfb6006b9f5882292
TeasdaleP <pteasdale55@gmail.com>
2021-07-26 13:29:45 +0100

    testing transactions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,11 @@ import { EffectsModule } from '@ngrx/effects';
 import { TransactionEffects } from './ngrx/effects/transaction.effects';
 import { BbUIModule } from './components/bb-ui/bb-ui.module';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    Ng2SearchPipeModule,
     HttpClientModule,
     StoreModule.forRoot(reducers),
     StoreDevtoolsModule.instrument(),
--- src/app/components/shared/transactions/transactions.component.spec.ts
+++ src/app/components/shared/transactions/transactions.component.spec.ts
@@ -1,32 +1,50 @@
+import { Pipe, PipeTransform } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Ng2SearchPipe } from 'ng2-search-filter';
+import { of } from 'rxjs';
+import { mockTransactions } from 'src/app/helpers/mock.data';
 
 import { TransactionsComponent } from './transactions.component';
 
 describe('Transactions Component', () => {
   let component: TransactionsComponent;
-  let fixture: ComponentFixture<TransactionsComponent>;
+
+  let mockSearchPipe = {
+    filter: () => ({})
+  }
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        TransactionsComponent,
-        // need to mock pipe here 
-      ]
-    })
-    .compileComponents();
+    component = new TransactionsComponent();
   }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TransactionsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-
-    // need to spy on filter
-  });
 
   it('Should create', () => {
     expect(component).toBeTruthy();
   });
 
+  it('Should get a list of transactions in an observable', () => {
+    component.transactions = of(mockTransactions);
 
+    component.transactions.subscribe((result) => {
+      expect(result.length).toBe(1);
+      expect(result[0].categoryCode).toEqual(mockTransactions[0].categoryCode);
+    });
+  });
+
+  it('Should have a title and an icon defined', () => {
+    expect(component.title).toBeUndefined();
+    expect(component.icon).toBeUndefined();
+
+    component.ngOnInit();
+
+    expect(component.title).toEqual('transactions list');
+    expect(component.icon).toEqual('fas fa-list');
+  });
+
+  it('Should be able to set search parameters in function', () => {
+    let search = 'search for transaction'
+    
+    expect(component.search).toEqual('');
+    component.filterBy(search);
+    expect(component.search).toEqual(search);
+  });
 });
